a9a351626cabefc6094bd1cf3189473de1e831cf
Zack Backmen <backmen@bk.ru>
2023-05-31 19:26:00 +0300

    Announcer

---
+++ Content.Server/SS220/Chat/Systems/AnnouncementSpokeEvent.cs
@@ -0,0 +1,33 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.Player;
+
+namespace Content.Server.SS220.Chat.Systems;
+
+public sealed class AnnouncementSpokeEvent : EntityEventArgs
+{
+    public readonly Filter Source;
+    public readonly string AnnouncementSound;
+    public readonly AudioParams AnnouncementSoundParams;
+    public readonly string Message;
+
+    public AnnouncementSpokeEvent(Filter source, string announcementSound, AudioParams announcementSoundParams, string message)
+    {
+        Source = source;
+        Message = message;
+        AnnouncementSound = announcementSound;
+        AnnouncementSoundParams = announcementSoundParams;
+    }
+}
+
+public sealed class RadioSpokeEvent : EntityEventArgs
+{
+    public readonly EntityUid Source;
+    public readonly string Message;
+
+
+    public RadioSpokeEvent(EntityUid source, string message)
+    {
+        Source = source;
+        Message = message;
+    }
+}
