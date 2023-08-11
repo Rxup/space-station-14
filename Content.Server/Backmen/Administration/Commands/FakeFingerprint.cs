c0de65511e37665e9a69cb277849e5d35799d472
Kayzel <43700376+KayzelW@users.noreply.github.com>
2023-08-11 18:21:07 +0300

    New Commands (#177)
    * New Commands
    
    * done

---
+++ Content.Server/Backmen/Administration/Commands/FakeFingerprint.cs
@@ -0,0 +1,56 @@
+﻿using Content.Server.Administration;
+using Content.Server.Administration.Logs;
+using Content.Server.Forensics;
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Server.Backmen.Administration.Commands;
+
+[AdminCommand(AdminFlags.Fun)]
+public sealed class FakeFingerprint : IConsoleCommand
+{
+    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+
+    public string Command => "fakefingerprints";
+
+    public string Description => "помещает отпечатки и ДНК жертвы на указанном объекте (волокна перчаток не берутся)";
+
+    public string Help => "fakefingerprints <playerUid> <targetEntityUid>";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if(args.Length != 2){
+            shell.WriteLine(Loc.GetString("shell-wrong-arguments-number"));
+            return;
+        }
+        if(!Enum.TryParse<EntityUid>(args[0], true, out var playerUid)){
+            shell.WriteLine(Loc.GetString("shell-invalid-entity-id"));
+            return;
+        }
+        if(!Enum.TryParse<EntityUid>(args[1], true, out var item)){
+            shell.WriteLine(Loc.GetString("shell-invalid-entity-id"));
+            return;
+        }
+        var f = _entityManager.EnsureComponent<ForensicsComponent>(item);
+        if (_entityManager.TryGetComponent<DnaComponent>(playerUid, out var dna))
+        {
+            f.DNAs.Add(dna.DNA);
+        }
+        if (_entityManager.TryGetComponent<FingerprintComponent>(playerUid, out var fingerprint))
+        {
+            f.Fingerprints.Add(fingerprint.Fingerprint ?? "");
+        }
+    }
+    public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        return args.Length switch
+        {
+            1 => CompletionResult.FromHintOptions(CompletionHelper.EntityUids(args[0], _entityManager),
+                "Персонаж"),
+            2 => CompletionResult.FromHintOptions(CompletionHelper.EntityUids(args[1], _entityManager),
+                "Целевой предмет"),
+            _ => CompletionResult.Empty
+        };
+    }
+}
---
+++ Content.Server/Backmen/Administration/Commands/MakeRimWorld.cs
@@ -0,0 +1,132 @@
+﻿using System.Linq;
+using System.Numerics;
+using Content.Server.Administration;
+using Content.Server.Administration.Logs;
+using Content.Server.Atmos;
+using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Decals;
+using Content.Server.Parallax;
+using Content.Shared.Administration;
+using Content.Shared.Atmos;
+using Content.Shared.Gravity;
+using Content.Shared.Parallax.Biomes;
+using Robust.Server.GameObjects;
+using Robust.Server.Maps;
+using Robust.Shared.Console;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+using Robust.Shared.Utility;
+
+namespace Content.Server.Backmen.Administration.Commands;
+
+[AdminCommand(AdminFlags.Mapping)]
+public sealed class MakeRimWorld : IConsoleCommand
+{
+    [Dependency] private readonly IEntitySystemManager _system = default!;
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
+    public string Command => "makerimworld";
+
+    public string Description => "помещает отпечатки и ДНК жертвы на указанном объекте (волокна перчаток не берутся)";
+
+    public string Help => "makerimworld";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var _prototypeManager = IoCManager.Resolve<IPrototypeManager>();
+        var _random = IoCManager.Resolve<IRobustRandom>();
+        if (!_prototypeManager.TryIndex<BiomeTemplatePrototype>("Grasslands", out var biomeTemplate))
+        {
+            return;
+        }
+
+        var mapId = _mapManager.NextMapId();
+        _mapManager.CreateMap(mapId);
+
+        var mapUid = _mapManager.GetMapEntityId(mapId);
+        var grid = _entityManager.EnsureComponent<MapGridComponent>(mapUid);
+        MetaDataComponent? metadata = null;
+
+        var biome = _entityManager.EnsureComponent<BiomeComponent>(mapUid);
+
+        var biomeSystem = _entityManager.System<BiomeSystem>();
+        biomeSystem.SetSeed(biome, _random.Next());
+        biomeSystem.SetTemplate(biome, biomeTemplate);
+        biomeSystem.AddMarkerLayer(biome, "Carps");
+        biomeSystem.AddMarkerLayer(biome, "OreTin");
+        biomeSystem.AddMarkerLayer(biome, "OreGold");
+        biomeSystem.AddMarkerLayer(biome, "OreSilver");
+        biomeSystem.AddMarkerLayer(biome, "OrePlasma");
+        biomeSystem.AddMarkerLayer(biome, "OreUranium");
+        biomeSystem.AddTemplate(biome, "Loot", _prototypeManager.Index<BiomeTemplatePrototype>("Caves"), 1);
+        _entityManager.Dirty(biome);
+
+        var gravity = _entityManager.EnsureComponent<GravityComponent>(mapUid);
+        gravity.Enabled = true;
+        _entityManager.Dirty(gravity, metadata);
+
+        var light = _entityManager.EnsureComponent<MapLightComponent>(mapUid);
+        light.AmbientLightColor = Color.FromHex("#2b3143");
+        _entityManager.Dirty(light, metadata);
+
+        var atmos = _entityManager.EnsureComponent<MapAtmosphereComponent>(mapUid);
+
+        var moles = new float[Atmospherics.AdjustedNumberOfGases];
+        moles[(int) Gas.Oxygen] = 21.824779f;
+        moles[(int) Gas.Nitrogen] = 82.10312f;
+
+        var mixture = new GasMixture(2500)
+        {
+            Temperature = 293.15f,
+            Moles = moles,
+        };
+
+        _entityManager.System<AtmosphereSystem>().SetMapAtmosphere(mapUid, false, mixture, atmos);
+        _entityManager.EnsureComponent<MapGridComponent>(mapUid);
+        var preloadArea = new Vector2(32f, 32f);
+        var mapPos = new MapCoordinates(new Vector2(0f, 0f), mapId);
+        var targetArea = new Box2(mapPos.Position - preloadArea, mapPos.Position + preloadArea);
+        biomeSystem.Preload(mapUid, biome, targetArea);
+        if (_system.GetEntitySystem<MapLoaderSystem>()
+            .TryLoad(mapId, "Maps/Backmen/Grids/RimWorldSpawn.yml", out var gridUid))
+        {
+            AttachedGrid( mapUid, gridUid.First());
+        }
+    }
+
+    private void AttachedGrid(EntityUid mapUid, EntityUid loadUid)
+    {
+        if (!_entityManager.TryGetComponent<BiomeComponent>(mapUid, out var biome) ||
+            !_entityManager.TryGetComponent<MapGridComponent>(mapUid, out var biomeGrid) ||
+            !_entityManager.TryGetComponent<MapGridComponent>(loadUid, out var loadGrid))
+        {
+            return;
+        }
+
+        var tiles = new List<(Vector2i Index, Tile Tile)>();
+        var aabb = new Box2(-32, -32, 32, 32);
+        var biomeSystem = _entityManager.System<BiomeSystem>();
+        for (var x = Math.Floor(aabb.Left); x <= Math.Ceiling(aabb.Right); x++)
+        {
+            for (var y = Math.Floor(aabb.Bottom); y <= Math.Ceiling(aabb.Top); y++)
+            {
+                var index = new Vector2i((int) x, (int) y);
+                var chunk = SharedMapSystem.GetChunkIndices(index, ChunkSize);
+
+                var mod = biome.ModifiedTiles.GetOrNew(chunk * ChunkSize);
+
+                if (!mod.Add(index) || !biomeSystem.TryGetBiomeTile(index, biome.Layers, biome.Noise, biomeGrid, out var tile))
+                    continue;
+
+                // If we flag it as modified then the tile is never set so need to do it ourselves.
+                tiles.Add((index, tile.Value));
+            }
+        }
+        biomeGrid.SetTiles(tiles);
+    }
+
+    protected const byte ChunkSize = 8;
+}
