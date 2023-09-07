50036ac2a6d5c28624b020b2c21f872d3376ed0f
Zack Backmen <backmen@bk.ru>
2023-09-07 20:27:56 +0300

    fix chat

--- Content.Client/Chat/Managers/ChatManager.cs
+++ Content.Client/Chat/Managers/ChatManager.cs
@@ -69,6 +69,10 @@ namespace Content.Client.Chat.Managers
                     _consoleHost.ExecuteCommand($"whisper \"{CommandParsing.Escape(str)}\"");
                     break;
 
+                case ChatSelectChannel.Telepathic:
+                    _consoleHost.ExecuteCommand($"tsay \"{CommandParsing.Escape(str)}\"");
+                    break;
+
                 default:
                     throw new ArgumentOutOfRangeException(nameof(channel), channel, null);
             }
