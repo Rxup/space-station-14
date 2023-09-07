84507b8fd79cc16ecb5f831ec53921dac186f2a6
Zack Backmen <backmen@bk.ru>
2023-09-07 23:29:46 +0300

    add station events, fix

---
+++ Content.Server/Backmen/Interaction/NoNormalInteractionComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server.Backmen.Interaction;
+
+/// <summary>
+/// Generic component for entities that can move around, emote, etc but just not physically interact with things.
+/// </summary>
+[RegisterComponent]
+public sealed partial class NoNormalInteractionComponent : Component
+{
+
+}
