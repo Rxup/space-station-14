b5c849f66648a71f10482d61c97c6b9c6bfc5324
Zack Backmen <backmen@bk.ru>
2023-09-26 17:00:19 +0300

    upd

---
+++ Content.Shared/Backmen/Laundry/SharedLaundry.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Backmen.Laundry;
+
+[RegisterComponent]
+public sealed partial class SharedWashingMachineComponent : Component
+{
+
+}
+
+[Serializable, NetSerializable]
+public enum WashingMachineVisualState : byte
+{
+    Broken,
+}
