2d374a12ed827cae625ee3dcacd33fd17ad36a34
Zack Backmen <backmen@bk.ru>
2023-10-08 16:51:34 +0300

    upd gpt

--- Content.Server/Backmen/Administration/Bwoink/Gpt/GptAhelpSystem.cs
+++ Content.Server/Backmen/Administration/Bwoink/Gpt/GptAhelpSystem.cs
@@ -3,50 +3,20 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Content.Server.Administration;
 using Content.Server.Administration.Managers;
+using Content.Server.Backmen.Administration.Bwoink.Gpt.Models;
 using Content.Shared.Administration;
 using Content.Shared.GameTicking;
 using Robust.Server.Player;
 using Robust.Shared.Configuration;
 using Robust.Shared.Console;
 using Robust.Shared.Network;
+using Robust.Shared.Players;
+using Robust.Shared.Utility;
 
-namespace Content.Server.Backmen;
-
-public enum GptUserDirection
-{
-    // ReSharper disable once InconsistentNaming
-    user,
-
-    // ReSharper disable once InconsistentNaming
-    assistant
-}
-
-public record GptMessage(
-    // ReSharper disable once InconsistentNaming
-    GptUserDirection role,
-    // ReSharper disable once InconsistentNaming
-    string message);
-
-#region ParamApi
-
-public record GptApiMessage(string role, string content);
-
-public record GptApiPacket(string model, GptApiMessage[] messages, float temperature = 0.7f)
-{
-    public bool stream = false;
-}
-
-#endregion
-
-#region ResponseApi
-
-public record GptResponseApiChoiceMsg(string content, string role);
-public record GptResponseApiChoice(int index, GptResponseApiChoiceMsg message);
-public record GptResponseApi(GptResponseApiChoice[] choices);
-
-#endregion
+namespace Content.Server.Backmen.Administration.Bwoink.Gpt;
 
 
 public sealed class GptAhelpSystem : EntitySystem
@@ -57,15 +27,17 @@ public sealed class GptAhelpSystem : EntitySystem
     [Dependency] private readonly IAdminManager _adminManager = default!;
     private readonly HttpClient _httpClient = new();
 
-    private Dictionary<NetUserId, List<GptMessage>> _history = new();
+    private Dictionary<NetUserId, GptUserInfo> _history = new();
 
     private bool _enabled = false;
     private string _apiUrl = "";
     private string _apiToken = "";
     private string _apiModel = "";
 
+    private const string BotName = "GptChat";
 
     private ISawmill _sawmill = default!;
+    private List<object> _gptFunctions = new();
     public override void Initialize()
     {
         base.Initialize();
@@ -80,12 +52,57 @@ public sealed class GptAhelpSystem : EntitySystem
         _console.RegisterCommand("ahelp_gpt", GptCommand, GptCommandCompletion);
     }
 
+    public void AddFunction(object model)
+    {
+        _gptFunctions.Add(model);
+    }
+
     private CompletionResult GptCommandCompletion(IConsoleShell shell, string[] args)
     {
         if (args.Length != 1)
             return CompletionResult.Empty;
 
         return CompletionResult.FromHintOptions(CompletionHelper.SessionNames(), "Пользователь");
+    }
+
+    private void SetTyping(NetUserId channel, bool enable)
+    {
+        // Non-admins can only ever type on their own ahelp, guard against fake messages
+        var update = new BwoinkPlayerTypingUpdated(channel, BotName, enable);
+
+        var admins = GetTargetAdmins();
+
+        foreach (var admin in admins)
+        {
+            RaiseNetworkEvent(update, admin);
+        }
+
+        if (_playerManager.TryGetSessionById(channel, out var session))
+        {
+            if (!admins.Contains(session.ConnectedClient))
+                RaiseNetworkEvent(update, session.ConnectedClient);
+        }
+    }
+
+    private async Task<(GptResponseApi? responseApi, string? err)> SendApiRequest(GptUserInfo history)
+    {
+        var payload = new GptApiPacket(_apiModel, history.GetMessagesForApi(), _gptFunctions,0.8f);
+        var request = await _httpClient.PostAsync($"{_apiUrl}chat/completions",
+            new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json"));
+
+        var response = await request.Content.ReadAsStringAsync();
+
+        if (!request.IsSuccessStatusCode)
+        {
+            _sawmill.Debug(JsonSerializer.Serialize(payload));
+            _sawmill.Debug(request.StatusCode.ToString());
+            _sawmill.Debug(response);
+            return (null, $"Ошибка! GptChat: {request.StatusCode} - {response}");
+        }
+
+        var info = JsonSerializer.Deserialize<GptResponseApi>(response);
+
+        return (info, null);
     }
 
     [AdminCommand(AdminFlags.Adminhelp)]
@@ -111,50 +128,104 @@ public sealed class GptAhelpSystem : EntitySystem
 
         var userId = player.UserId;
 
-        if (!_history.ContainsKey(userId) || _history[userId].Count == 0)
-        {
-            shell.WriteError("Пользователь не писал сообщений!");
-            return;
-        }
-        if (_history[userId].Last().role != GptUserDirection.user)
+        if (!_history.ContainsKey(userId) || _history[userId].Messages.Count == 0)
         {
             shell.WriteError("Пользователь не писал сообщений!");
             return;
         }
 
-        var payload = new GptApiPacket(_apiModel, _history[userId].Select(x=>new GptApiMessage(x.role.ToString(), x.message)).ToArray(),0.8f);
-        var request = await _httpClient.PostAsync($"{_apiUrl}chat/completions",
-            new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json"));
+        var history = _history[userId];
 
-        var response = await request.Content.ReadAsStringAsync();
-
-        if (!request.IsSuccessStatusCode)
+        if (history.Lock.IsWriteLockHeld)
         {
-            _sawmill.Debug(JsonSerializer.Serialize(payload));
-            _sawmill.Debug(request.StatusCode.ToString());
-            _sawmill.Debug(response);
-            shell.WriteError($"Ошибка! GptChat: {request.StatusCode} - {response}");
+            shell.WriteError("Уже в процессе!");
             return;
         }
 
-        var info = JsonSerializer.Deserialize<GptResponseApi>(response);
+        if (!history.IsCanAnswer())
+        {
+            shell.WriteError("Пользователь не писал сообщений!");
+            return;
+        }
 
+        history.Lock.EnterWriteLock();
+        try
+        {
+            SetTyping(userId, true);
+
+            await ProcessRequest(shell, userId, history);
+
+            shell.WriteLine("ГОТОВО!");
+        }
+        finally
+        {
+            history.Lock.ExitWriteLock();
+        }
+    }
+
+    private async Task ProcessRequest(IConsoleShell shell, NetUserId userId, GptUserInfo history)
+    {
+        var (info, err) = await SendApiRequest(history);
+        if (!string.IsNullOrEmpty(err))
+        {
+            shell.WriteError(err);
+            return;
+        }
         if (info == null)
         {
             shell.WriteError($"Ошибка! GptChat: ответ = null");
             return;
         }
 
+        await ProcessResponse(shell, userId, history, info);
+    }
+
+    private async Task ProcessResponse(IConsoleShell shell, NetUserId userId, GptUserInfo history, GptResponseApi info)
+    {
         foreach (var gptMsg in info.choices)
         {
-
             if (!_history.ContainsKey(userId))
             {
                 return;
             }
-            _history[userId].Add(new GptMessage(GptUserDirection.assistant, gptMsg.message.content));
 
-            var bwoinkText = $"[color=lightblue]GptChat[/color]: {gptMsg.message.content}";
+            if (gptMsg.finish_reason == "function_call")
+            {
+                await ProcessFunctionCall(shell, userId, history, gptMsg);
+                break;
+            }
+            else if (gptMsg.finish_reason == "stop")
+            {
+                await ProcessChatResponse(shell, userId, history, gptMsg);
+                break;
+            }
+        }
+    }
+
+    private async Task ProcessFunctionCall(IConsoleShell shell, NetUserId userId, GptUserInfo history, GptResponseApiChoice msg)
+    {
+        DebugTools.AssertNotNull(msg.message.function_call);
+        _sawmill.Debug("FunctionCall {0} with {1}",msg.message.function_call!.name, msg.message.function_call!.arguments);
+
+        var ev = new EventGptFunctionCall(shell,userId,history,msg);
+        RaiseLocalEvent(ev);
+
+        if (!ev.Handled)
+        {
+            history.Add(new GptMessageFunction(msg.message.function_call!.name));
+        }
+
+        await ProcessRequest(shell, userId, history);
+    }
+
+    private async Task ProcessChatResponse(IConsoleShell shell, NetUserId userId, GptUserInfo history, GptResponseApiChoice gptMsg)
+    {
+        try
+        {
+            DebugTools.AssertNotNull(gptMsg.message.content);
+            history.Messages.Add(new GptMessageChat(GptUserDirection.assistant, gptMsg.message.content!));
+
+            var bwoinkText = $"[color=lightblue]{BotName}[/color]: {gptMsg.message.content}";
 
             var msg = new SharedBwoinkSystem.BwoinkTextMessage(userId, SharedBwoinkSystem.SystemUserId, bwoinkText);
 
@@ -172,10 +243,10 @@ public sealed class GptAhelpSystem : EntitySystem
                     RaiseNetworkEvent(msg, session.ConnectedClient);
             }
         }
-
-
-
-        shell.WriteLine("ГОТОВО!");
+        finally
+        {
+            SetTyping(userId, false);
+        }
     }
 
     // Returns all online admins with AHelp access
@@ -237,17 +308,25 @@ public sealed class GptAhelpSystem : EntitySystem
             return;
         }
 
-        if (!_history.ContainsKey(messageUserId))
-        {
-            _history.Add(messageUserId, new List<GptMessage>());
-        }
+        _history.TryAdd(messageUserId, new GptUserInfo());
 
-        _history[messageUserId]
-            .Add(new GptMessage(personalChannel ? GptUserDirection.user : GptUserDirection.assistant, escapedText));
-
-        if (_history[messageUserId].Count > 5)
-        {
-            _history[messageUserId].RemoveRange(0, _history[messageUserId].Count - 5);
-        }
+        _history[messageUserId].Add(new GptMessageChat(personalChannel ? GptUserDirection.user : GptUserDirection.assistant, escapedText));
     }
+}
+
+public sealed class EventGptFunctionCall : HandledEntityEventArgs
+{
+    public IConsoleShell Shell { get; }
+    public NetUserId UserId { get; }
+    public GptUserInfo History { get; }
+    public GptResponseApiChoice Msg { get; }
+
+    public EventGptFunctionCall(IConsoleShell shell, NetUserId userId, GptUserInfo history, GptResponseApiChoice msg)
+    {
+        Shell = shell;
+        UserId = userId;
+        History = history;
+        Msg = msg;
+    }
+
 }
