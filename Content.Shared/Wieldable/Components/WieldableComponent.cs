1a4f7a6f963ea3dd50d8b1cf950b0cac537898ce
Kayzel <kapitonovroman5@gmail.com>
2023-11-05 10:09:00 +0300

    fixes

--- Content.Shared/Wieldable/Components/WieldableComponent.cs
+++ Content.Shared/Wieldable/Components/WieldableComponent.cs
@@ -30,6 +30,9 @@ public sealed partial class WieldableComponent : Component
     public string? WieldedInhandPrefix = "wielded";
 
     public string? OldInhandPrefix = null;
+
+    [DataField("wieldTime")]
+    public double WieldTime; //nothing to do
 }
 
 [Serializable, NetSerializable]
