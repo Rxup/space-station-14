d5d7c0d05de7902301873d40c078ae829cbf9958
FilinSep <117042393+FilinSep@users.noreply.github.com>
2023-11-18 18:42:53 +0300

    fuk I forgot

---
+++ Content.Shared/Backmen/Magicians/MagiciansComponent.cs
@@ -0,0 +1,17 @@
+using Content.Shared.Chemistry.Components;
+using Content.Shared.Chemistry.Reagent;
+using Content.Shared.FixedPoint;
+using Content.Shared.Maps;
+using Content.Shared.Store;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Shared.Backmen.Magicians;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class MagiciansComponent : Component
+{
+ 
+}
