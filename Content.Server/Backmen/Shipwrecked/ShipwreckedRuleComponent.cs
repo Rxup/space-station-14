a2fa0e70fecc51948a35675edac15ac998b196ab
Zack Backmen <backmen@bk.ru>
2023-11-23 01:42:27 +0300

    fix

--- Content.Server/Backmen/Shipwrecked/ShipwreckedRuleComponent.cs
+++ Content.Server/Backmen/Shipwrecked/ShipwreckedRuleComponent.cs
@@ -70,9 +70,6 @@ public sealed partial class ShipwreckedRuleComponent : Component
 
 #region Live Data
 
-    [ViewVariables]
-    public bool isMapLoaded = false;
-
     /// <summary>
     /// A list of all survivors and their player sessions.
     /// </summary>
