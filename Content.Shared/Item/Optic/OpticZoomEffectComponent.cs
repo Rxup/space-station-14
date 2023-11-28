e592196c498614bb97c287a98db155406827ae54
Zack Backmen <backmen@bk.ru>
2023-11-29 00:50:28 +0300

    Revert "Delete OpticZoomEffectComponent.cs"
    This reverts commit 172f49bd1eaa6121396afce34cd574f575b403a0.

---
+++ Content.Shared/Item/Optic/OpticZoomEffectComponent.cs
@@ -0,0 +1,15 @@
+﻿using Content.Shared.Actions;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Item.Optic;
+
+[RegisterComponent]
+public sealed partial class OpticZoomEffectComponent : Component
+{
+
+}
+
+public sealed partial class OpticZoomEffectActionEvent : InstantActionEvent
+{
+
+}
