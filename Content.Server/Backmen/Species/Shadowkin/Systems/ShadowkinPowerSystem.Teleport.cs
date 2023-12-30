84a9730784963e82ec5ca2f170d5d0a890279961
Rxup <backmen@bk.ru>
2023-12-30 21:07:14 +0300

    nerf (#406)

--- Content.Server/Backmen/Species/Shadowkin/Systems/ShadowkinPowerSystem.Teleport.cs
+++ Content.Server/Backmen/Species/Shadowkin/Systems/ShadowkinPowerSystem.Teleport.cs
@@ -5,11 +5,13 @@ using Content.Server.Pulling;
 using Content.Server.Backmen.Species.Shadowkin.Components;
 using Content.Server.Backmen.Species.Shadowkin.Events;
 using Content.Shared.Actions;
+using Content.Shared.Backmen.Abilities.Psionics;
 using Content.Shared.Backmen.Species.Shadowkin.Components;
 using Content.Shared.Cuffs.Components;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Pulling.Components;
 using Content.Shared.Backmen.Species.Shadowkin.Components;
+using Robust.Server.GameObjects;
 using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Prototypes;
@@ -56,12 +58,12 @@ public sealed class ShadowkinTeleportSystem : EntitySystem
 
         // Don't activate abilities if handcuffed
         // TODO: Something like the Psionic Headcage to disable powers for Shadowkin
-        if (HasComp<HandcuffComponent>(args.Performer))
+        if (HasComp<HandcuffComponent>(args.Performer) || HasComp<PsionicInsulationComponent>(args.Performer))
             return;
 
 
         var transform = Transform(args.Performer);
-        if (transform.MapID != args.Target.GetMapId(EntityManager))
+        if (transform.MapID != args.Target.GetMapId(EntityManager) || transform.GridUid == null)
             return;
 
         SharedPullableComponent? pullable = null; // To avoid "might not be initialized when accessed" warning
@@ -76,7 +78,7 @@ public sealed class ShadowkinTeleportSystem : EntitySystem
 
         // Teleport the performer to the target
         _transform.SetCoordinates(args.Performer, args.Target);
-        transform.AttachToGridOrMap();
+        _transform.AttachToGridOrMap(args.Performer);
 
         if (pullable != null && puller != null)
         {
@@ -86,7 +88,7 @@ public sealed class ShadowkinTeleportSystem : EntitySystem
             // Teleport the pulled entity to the target
             // TODO: Relative position to the performer
             _transform.SetCoordinates(puller.Pulling.Value, args.Target);
-            pulledTransform.AttachToGridOrMap();
+            _transform.AttachToGridOrMap(puller.Pulling!.Value);
 
             // Resume pulling
             // TODO: This does nothing? // This does things sometimes, but the client never knows
