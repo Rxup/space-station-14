0628311a8ae536c47608fb5f1c4d4c85efb06d48
dttric <87319664+dttric@users.noreply.github.com>
2024-01-25 23:32:51 +0700

    контрибы

--- Tools/dump_github_contributors.ps1
+++ Tools/dump_github_contributors.ps1
@@ -27,7 +27,7 @@ function load_contribs([string] $repo)
 }
 
 $engineJson = load_contribs("space-wizards/RobustToolbox")
-$contentJson = load_contribs("space-wizards/space-station-14")
+$contentJson = load_contribs("astroplex-ss14/space-station-14")
 
 ($engineJson).login + ($contentJson).login `
     | select -unique `
