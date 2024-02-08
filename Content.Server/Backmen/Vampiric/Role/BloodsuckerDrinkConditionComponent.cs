acf3fbbaad467cfd43edeed4e1c6239943133d58
Rxup <backmen@bk.ru>
2024-02-08 23:58:24 +0300

    vpm (#455)
    * vpm
    
    * plehUPD
    
    * roundend over mind
    
    * updPLEH
    
    * clean
    
    ---------
    
    Co-authored-by: Kirillcas <natasafrolova3825@gmail.com>

---
+++ Content.Server/Backmen/Vampiric/Role/BloodsuckerDrinkConditionComponent.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.FixedPoint;
+
+namespace Content.Server.Backmen.Vampiric.Role;
+
+[RegisterComponent]
+public sealed partial class BloodsuckerDrinkConditionComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    public float Goal = 0;
+    [DataField("min")]
+    public int MinGoal = 400;
+    [DataField("max")]
+    public int MaxGoal = 800;
+
+    public string ObjectiveText = "objective-bloodsucker-drink-name";
+    public string DescriptionText = "objective-bloodsucker-drink-desc";
+}
---
+++ Content.Shared/Backmen/Vampiric/BkmVampireComponent.cs
@@ -0,0 +1,30 @@
+﻿using Content.Shared.Actions;
+using Content.Shared.Antag;
+using Content.Shared.DoAfter;
+using Content.Shared.FixedPoint;
+using Content.Shared.StatusIcon;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Backmen.Vampiric;
+
+[RegisterComponent]
+[NetworkedComponent]
+public sealed partial class BkmVampireComponent : Component, IAntagStatusIconComponent
+{
+    public ProtoId<StatusIconPrototype> StatusIcon { get; set; } = "VampireFaction";
+    public bool IconVisibleToGhost { get; set; } = true;
+
+    public EntityUid? ActionNewVamp;
+    public ProtoId<EntityPrototype> NewVamp = "ActionConvertToVampier";
+}
+
+public sealed partial class InnateNewVampierActionEvent : EntityTargetActionEvent
+{
+}
+
+[Serializable, NetSerializable]
+public sealed partial class InnateNewVampierDoAfterEvent : SimpleDoAfterEvent
+{
+}
