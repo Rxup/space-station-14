dc671ea04befb36beb35b1e93d7da5932ded61ad
Zack Backmen <backmen@bk.ru>
2024-03-24 23:11:22 +0300

    fix blob ghost role, new ui ghost roller, ghost roller can be cancel

--- Content.Shared/Backmen/CCVar/CCVars.cs
+++ Content.Shared/Backmen/CCVar/CCVars.cs
@@ -164,6 +164,6 @@ public sealed class CCVars
     /// <summary>
     /// the time that will be given to throw a number to vote for the ghost role
     /// </summary>
-    public static readonly CVarDef<float> GhostRollerTime =
-        CVarDef.Create("ghost.roller_time", 10f, CVar.REPLICATED | CVar.SERVER);
+    public static readonly CVarDef<int> GhostRollerTime =
+        CVarDef.Create("ghost.roller_time", 20, CVar.REPLICATED | CVar.SERVER);
 }
