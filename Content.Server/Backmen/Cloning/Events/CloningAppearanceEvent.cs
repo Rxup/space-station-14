c79f36dbbf3cbf4a1296c544959817acae3f229d
Rxup <backmen@bk.ru>
2024-05-04 23:40:23 +0300

    ghost role cluwne (#596)

---
+++ Content.Server/Backmen/Cloning/Events/CloningAppearanceEvent.cs
@@ -0,0 +1,13 @@
+﻿using Content.Server.Backmen.Cloning.Components;
+using Robust.Shared.Map;
+using Robust.Shared.Player;
+
+namespace Content.Server.Backmen.Cloning.Events;
+
+public sealed class CloningAppearanceEvent : EntityEventArgs
+{
+    public ICommonSession Player = default!;
+    public CloningAppearanceComponent Component = default!;
+    public EntityCoordinates Coords { get; set; }
+    public EntityUid? StationUid { get; set; }
+}
