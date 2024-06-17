49a7f9ff78db22d4bbd659e2f7179067bc4ef156
Roudenn <romabond091@gmail.com>
2024-06-17 22:17:44 +0300

    Update SpecForceTest.cs

--- Content.IntegrationTests/Tests/Backmen/SpecForce/SpecForceTest.cs
+++ Content.IntegrationTests/Tests/Backmen/SpecForce/SpecForceTest.cs
@@ -65,64 +65,72 @@ public sealed class SpecForceTest
             // Now check if there are any GhostRoles and SpecForces
             Assert.That(entMan.Count<GhostRoleComponent>(), Is.GreaterThan(0));
             Assert.That(entMan.Count<SpecForceComponent>(), Is.GreaterThan(0));
+        }
 
-            // Get all ghost roles and take them over.
-            var ghostRoles = entMan.EntityQuery<GhostRoleComponent>().ToList();
-            foreach (var ghostRoleComp in ghostRoles)
+        // Get all ghost roles and take them over.
+        var ghostRoles = entMan.EntityQuery<GhostRoleComponent>().ToList();
+        foreach (var ghostRoleComp in ghostRoles)
+        {
+            // Take the ghost role.
+            await server.WaitPost(() =>
             {
-                // Take the ghost role.
-                await server.WaitPost(() =>
-                {
-                    var id = entMan.GetComponent<GhostRoleComponent>(ghostRoleComp.Owner).Identifier;
-                    entMan.EntitySysManager.GetEntitySystem<GhostRoleSystem>().Takeover(session, id);
-                });
+                var id = entMan.GetComponent<GhostRoleComponent>(ghostRoleComp.Owner).Identifier;
+                entMan.EntitySysManager.GetEntitySystem<GhostRoleSystem>().Takeover(session, id);
+            });
 
-                // Check that role name and description is valid.
-                // We must wait because GhostRoleComponent uses Localisation methods in get property
-                await server.WaitPost(() =>
-                {
-                    Assert.That(ghostRoleComp.RoleName, Is.Not.EqualTo("Unknown"));
-                    Assert.That(ghostRoleComp.RoleDescription, Is.Not.EqualTo("Unknown"));
-                });
+            // Check that role name and description is valid.
+            // We must wait because GhostRoleComponent uses Localisation methods in get property
+            await server.WaitPost(() =>
+            {
+                Assert.That(ghostRoleComp.RoleName, Is.Not.EqualTo("Unknown"));
+                Assert.That(ghostRoleComp.RoleDescription, Is.Not.EqualTo("Unknown"));
+            });
 
-                // Check player got attached to ghost role.
-                var player = session.AttachedEntity!.Value;
-                await pair.RunTicksSync(10);
-                var newMindId = session.ContentData()!.Mind!.Value;
-                var newMind = entMan.GetComponent<MindComponent>(newMindId);
-                Assert.That(newMind.OwnedEntity, Is.EqualTo(player));
-                Assert.That(newMind.VisitingEntity, Is.Null);
-                Assert.That(entMan.HasComponent<GhostComponent>(player), Is.False, "Player is still a ghost after attaching to an entity!");
+            // Check player got attached to ghost role.
+            var player = session.AttachedEntity!.Value;
+            await pair.RunTicksSync(10);
+            var newMindId = session.ContentData()!.Mind!.Value;
+            var newMind = entMan.GetComponent<MindComponent>(newMindId);
+            Assert.That(newMind.OwnedEntity, Is.EqualTo(player));
+            Assert.That(newMind.VisitingEntity, Is.Null);
+            Assert.That(entMan.HasComponent<GhostComponent>(player),
+                Is.False,
+                "Player is still a ghost after attaching to an entity!");
 
-                await server.WaitPost(() => specForceSystem.Log.Info("Player attaching succeeded, starting side checks."));
+            await server.WaitPost(() =>
+                specForceSystem.Log.Info("Player attaching succeeded, starting side checks."));
 
-                // SpecForce should have at least 3 items in their inventory slots.
-                var enumerator = invSys.GetSlotEnumerator(player);
-                var total = 0;
-                while (enumerator.NextItem(out _))
-                {
-                    total++;
-                }
-                Assert.That(total, Is.GreaterThan(3), $"SpecForce {entMan.ToPrettyString(player)} in team {teamProto.ID} has less than 3 items in inventory: {total}.");
-
-                // Finally check if The Great NT Evil-Fighter Agent passed basic training and figured out how to breathe.
-                await pair.RunTicksSync(10);
-                var totalSeconds = 30;
-                var totalTicks = (int) Math.Ceiling(totalSeconds / server.Timing.TickPeriod.TotalSeconds);
-                int increment = 5;
-                var resp = entMan.GetComponent<RespiratorComponent>(player);
-                var damage = entMan.GetComponent<DamageableComponent>(player);
-                for (var tick = 0; tick < totalTicks; tick += increment)
-                {
-                    await pair.RunTicksSync(increment);
-                    Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
-                    Assert.That(damage.TotalDamage, Is.EqualTo(FixedPoint2.Zero), $"SpecForce {entMan.ToPrettyString(player)} in team {teamProto.ID} is stupid and don't know how to breathe!");
-                }
-
-                // Use the ghost command at the end and move on
-                conHost.ExecuteCommand("ghost");
-                await pair.RunTicksSync(5);
+            // SpecForce should have at least 3 items in their inventory slots.
+            var enumerator = invSys.GetSlotEnumerator(player);
+            var total = 0;
+            while (enumerator.NextItem(out _))
+            {
+                total++;
             }
+
+            Assert.That(total,
+                Is.GreaterThan(3),
+                $"SpecForce {entMan.ToPrettyString(player)} has less than 3 items in inventory: {total}.");
+
+            // Finally check if The Great NT Evil-Fighter Agent passed basic training and figured out how to breathe.
+            await pair.RunTicksSync(10);
+            var totalSeconds = 30;
+            var totalTicks = (int) Math.Ceiling(totalSeconds / server.Timing.TickPeriod.TotalSeconds);
+            int increment = 5;
+            var resp = entMan.GetComponent<RespiratorComponent>(player);
+            var damage = entMan.GetComponent<DamageableComponent>(player);
+            for (var tick = 0; tick < totalTicks; tick += increment)
+            {
+                await pair.RunTicksSync(increment);
+                Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
+                Assert.That(damage.TotalDamage,
+                    Is.EqualTo(FixedPoint2.Zero),
+                    $"SpecForce {entMan.ToPrettyString(player)} is stupid and don't know how to breathe!");
+            }
+
+            // Use the ghost command at the end and move on
+            conHost.ExecuteCommand("ghost");
+            await pair.RunTicksSync(5);
         }
 
         await pair.CleanReturnAsync();
