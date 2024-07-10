ef280954b720b5434a717df06bc9fc6fe2c0eb08
Kayzel <kapitonovroman5@gmail.com>
2024-07-10 17:51:51 +0300

    perms upd

--- Content.Server/Whitelist/WhitelistCommands.cs
+++ Content.Server/Whitelist/WhitelistCommands.cs
@@ -11,7 +11,7 @@ using Content.Server.Players; // backmen: whitelist
 
 namespace Content.Server.Whitelist;
 
-[AdminCommand(AdminFlags.Permissions)]
+[AdminCommand(AdminFlags.WhiteList)]
 public sealed class AddWhitelistCommand : LocalizedCommands
 {
     public override string Command => "whitelistadd";
@@ -62,7 +62,7 @@ public sealed class AddWhitelistCommand : LocalizedCommands
     }
 }
 
-[AdminCommand(AdminFlags.Permissions)]
+[AdminCommand(AdminFlags.WhiteList)]
 public sealed class RemoveWhitelistCommand : LocalizedCommands
 {
     public override string Command => "whitelistremove";
@@ -113,7 +113,7 @@ public sealed class RemoveWhitelistCommand : LocalizedCommands
     }
 }
 
-[AdminCommand(AdminFlags.Permissions)]
+[AdminCommand(AdminFlags.Host)]
 public sealed class KickNonWhitelistedCommand : LocalizedCommands
 {
     public override string Command => "kicknonwhitelisted";
