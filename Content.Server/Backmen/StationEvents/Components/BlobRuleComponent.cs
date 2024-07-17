cc430e1cfa8e745934256301c5d7cab27e95c1de
Roudenn <romabond091@gmail.com>
2024-07-17 21:33:09 +0300

    Max 2 blob carriers in BlobSpawn event
    Теперь блоб мышь не может вылезти во время ивента с вентиляциями, максимальное количество блобов для спавна уменьшено с 3 до 2, а сам BlobSpawnRule появляется лишь 1 раз за раунд

--- Content.Server/Backmen/StationEvents/Components/BlobRuleComponent.cs
+++ Content.Server/Backmen/StationEvents/Components/BlobRuleComponent.cs
@@ -10,15 +10,12 @@ public sealed partial class BlobSpawnRuleComponent : Component
     [DataField("carrierBlobProtos", required: true, customTypeSerializer: typeof(PrototypeIdListSerializer<EntityPrototype>)), ViewVariables(VVAccess.ReadWrite)]
     public List<string> CarrierBlobProtos = new()
     {
-        "MobMouseCancer"
-        //"MobMouse",
-        //"MobMouse1",
-        //"MobMouse2"
+        "SpawnPointGhostBlobRat"
     };
 
     [ViewVariables(VVAccess.ReadOnly), DataField("playersPerCarrierBlob")]
     public int PlayersPerCarrierBlob = 30;
 
     [ViewVariables(VVAccess.ReadOnly), DataField("maxCarrierBlob")]
-    public int MaxCarrierBlob = 3;
+    public int MaxCarrierBlob = 2;
 }
