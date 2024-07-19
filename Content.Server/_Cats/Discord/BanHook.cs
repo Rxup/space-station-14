8b7d49c524fad518a807307fb69d3e6541364ba9
CrimeMoot <wakeafa@gmail.com>
2024-07-20 03:11:08 +0500

    Update BanHook.cs

--- Content.Server/_Cats/Discord/BanHook.cs
+++ Content.Server/_Cats/Discord/BanHook.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks
 using System.Text.RegularExpressions;
 using Content.Server.Discord;
 using Content.Shared.CCVar;
