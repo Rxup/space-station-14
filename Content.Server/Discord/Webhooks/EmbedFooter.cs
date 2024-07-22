7178472809499e0233f6c43856e5ba7e9b1246ee
CrimeMoot <wakeafa@gmail.com>
2024-07-23 01:49:23 +0500

    add

---
+++ Content.Server/Discord/Webhooks/EmbedFooter.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+
+namespace Content.Server.Discord.Webhooks
+{
+    public struct EmbedFooter
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = "";
+
+        [JsonPropertyName("icon_url")]
+        public string? IconUrl { get; set; }
+
+        public EmbedFooter()
+        {
+        }
+    }
+}
---
+++ Content.Server/Discord/Webhooks/WebhookPayload.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+
+namespace Content.Server.Discord.Webhooks
+{
+    public struct WebhookPayload
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; } = "";
+
+        [JsonPropertyName("avatar_url")]
+        public string? AvatarUrl { get; set; } = "";
+
+        [JsonPropertyName("embeds")]
+        public List<Embed>? Embeds { get; set; } = null;
+
+        [JsonPropertyName("allowed_mentions")]
+        public Dictionary<string, string[]> AllowedMentions { get; set; } =
+            new()
+            {
+                { "parse", Array.Empty<string>() },
+            };
+
+        public WebhookPayload()
+        {
+        }
+    }
+}
