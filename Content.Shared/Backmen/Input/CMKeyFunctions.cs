055fa0bc925128c2b2ce0a01d9260ff3910f7ffe
no_mad <110369449+nomad0260@users.noreply.github.com>
2024-08-21 00:01:25 +0900

    Shotguns! (#721)
    * ported systems
    
    * locale
    
    * yml
    
    * перевод

---
+++ Content.Shared/Backmen/Input/CMKeyFunctions.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.Input;
+
+namespace Content.Shared.Backmen.Input;
+
+[KeyFunctions]
+public sealed class CMKeyFunctions
+{
+    public static readonly BoundKeyFunction CMUniqueAction = "CMUniqueAction";
+}
