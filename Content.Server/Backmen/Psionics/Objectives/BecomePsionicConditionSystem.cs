3b4bab88afb61a3d73ad08ba107922f0f5169e0a
Zack Backmen <backmen@bk.ru>
2024-08-30 14:13:00 +0300

    cleanup + psi traitor

---
+++ Content.Server/Backmen/Psionics/Objectives/BecomePsionicConditionSystem.cs
@@ -0,0 +1,26 @@
+﻿using Content.Server.Backmen.Psionics.Objectives.Components;
+using Content.Shared.Backmen.Abilities.Psionics;
+using Content.Shared.Mind;
+using Content.Shared.Objectives.Components;
+
+namespace Content.Server.Backmen.Psionics.Objectives;
+
+public sealed class BecomePsionicConditionSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BecomePsionicConditionComponent, ObjectiveGetProgressEvent>(OnGetProgress);
+    }
+
+    private void OnGetProgress(Entity<BecomePsionicConditionComponent> ent, ref ObjectiveGetProgressEvent args)
+    {
+        args.Progress = GetProgress(args.Mind);
+    }
+
+    private float GetProgress(MindComponent mind)
+    {
+        return HasComp<PsionicComponent>(mind.OwnedEntity) ? 1 : 0;
+    }
+}
