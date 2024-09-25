159a0fbd6f254921a50e3d8b2712098d1ddcfcd7
CrimeMoot <wakeafa@gmail.com>
2024-09-25 19:54:06 +0500

    null

--- Content.Server/Access/Systems/IdCardSystem.cs
+++ Content.Server/Access/Systems/IdCardSystem.cs
@@ -108,7 +108,7 @@ public sealed class IdCardSystem : SharedIdCardSystem
             return true;
 
         id.JobColor = color.Value;
-        Dirty<IdCardComponent>(id);
+        Dirty(id, null);
 
         if (player != null)
         {
