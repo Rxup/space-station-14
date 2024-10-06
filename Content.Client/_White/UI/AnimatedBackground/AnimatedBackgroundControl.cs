474c137f5fa99a66768c0efb7227a050458a5794
Roudenn <romabond091@gmail.com>
2024-10-06 19:33:22 +0300

    Migrate RMC14 + WD Sprites

--- Content.Client/_White/UI/AnimatedBackground/AnimatedBackgroundControl.cs
+++ Content.Client/_White/UI/AnimatedBackground/AnimatedBackgroundControl.cs
@@ -15,7 +15,7 @@ public sealed class AnimatedBackgroundControl : TextureRect
     [Dependency] private readonly IClyde _clyde = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
 
-    private string _rsiPath = "/Textures/_White/LobbyScreens/native.rsi";
+    private string _rsiPath = "/Textures/Backmen/LobbyScreens/native.rsi";
     public RSI? _RSI;
     private const int States = 1;
 
