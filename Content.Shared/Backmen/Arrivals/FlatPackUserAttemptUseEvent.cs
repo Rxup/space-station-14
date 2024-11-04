cab1441217c200e561f029e3f3db2ef7a2d06b2d
Zack Backmen <backmen@bk.ru>
2024-11-04 04:21:06 +0300

    ArrivalsProtect update

---
+++ Content.Shared/Backmen/Arrivals/FlatPackUserAttemptUseEvent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Backmen.Arrivals;
+
+[ByRefEvent]
+public struct FlatPackUserAttemptUseEvent(EntityUid user, EntProtoId? itemToSpawn, EntityCoordinates coords)
+{
+    public readonly EntityUid User = user;
+    public readonly EntProtoId? ItemToSpawn = itemToSpawn;
+    public readonly EntityCoordinates Coords = coords;
+    public bool Cancelled = false;
+}
