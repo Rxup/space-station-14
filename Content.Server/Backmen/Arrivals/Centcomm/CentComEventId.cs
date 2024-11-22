5cb5033d4180c1a467b2b6dc5e41fa356127dacb
Rxup <backmen@bk.ru>
2024-11-22 13:53:37 +0300

    ЦК v2 (#928)
    * v2
    
    * фикс
    
    * Update BKCCSecure.yml

---
+++ Content.Server/Backmen/Arrivals/Centcomm/CentComEventId.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server.Backmen.Arrivals.CentComm;
+
+public enum CentComEventId : int
+{
+    Noop = 0,
+    AddWorker,
+    AddOperator,
+    AddSecurity,
+    AddCargo
+}
---
+++ Content.Server/Backmen/Arrivals/Centcomm/CentCommSpawnSystem.cs
@@ -0,0 +1,101 @@
+﻿using Content.Server.Spawners.Components;
+using Content.Server.Station.Components;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+
+namespace Content.Server.Backmen.Arrivals.CentComm;
+
+public sealed class CentCommSpawnSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StationCentCommDirectorComponent, CentCommEvent>(OnCentCommEvent);
+    }
+
+    private void OnCentCommEvent(Entity<StationCentCommDirectorComponent> ent, ref CentCommEvent args)
+    {
+        if(args.Handled)
+            return;
+
+        switch (args.EventId)
+        {
+            case CentComEventId.AddWorker:
+                args.Handled = true;
+
+                AddWorker(args.Station);
+                break;
+            case CentComEventId.AddOperator:
+                args.Handled = true;
+
+                AddOperator(args.Station);
+                break;
+            case CentComEventId.AddSecurity:
+                args.Handled = true;
+
+                AddSecurity(args.Station);
+                break;
+            case CentComEventId.AddCargo:
+                args.Handled = true;
+
+                AddCargo(args.Station);
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void SpawnEntity(EntityUid station, string protoId)
+    {
+        var point = FindSpawnPoint(station);
+        if (point == null)
+        {
+            Log.Warning($"Can't find spawn point for {station}");
+            return;
+        }
+        Spawn(protoId, point.Value);
+    }
+
+    [ValidatePrototypeId<EntityPrototype>]
+    private const string WorkerProto = "SpawnPointCMBKCCAssistant";
+    private void AddWorker(EntityUid station) => SpawnEntity(station, WorkerProto);
+
+    [ValidatePrototypeId<EntityPrototype>]
+    private const string OperatorProto = "SpawnPointCMBKCCOperator";
+    private void AddOperator(EntityUid station) => SpawnEntity(station, OperatorProto);
+
+    [ValidatePrototypeId<EntityPrototype>]
+    private const string SecurityProto = "SpawnPointCMBKCCSecOfficer";
+    private void AddSecurity(EntityUid station) => SpawnEntity(station, SecurityProto);
+
+    [ValidatePrototypeId<EntityPrototype>]
+    private const string CargoProto = "SpawnPointCMBKCCCargo";
+    private void AddCargo(EntityUid station) => SpawnEntity(station, CargoProto);
+
+    private EntityCoordinates? FindSpawnPoint(EntityUid station)
+    {
+        var stationData = CompOrNull<StationDataComponent>(station);
+        if (stationData == null)
+            return null;
+
+        var stationGrids = stationData.Grids;
+
+        var result = new List<EntityCoordinates>();
+
+        var q = EntityQueryEnumerator<SpawnPointComponent,TransformComponent>();
+        while (q.MoveNext(out var uid, out var spawnPoint, out var transform))
+        {
+            if(spawnPoint.SpawnType != SpawnPointType.LateJoin || transform.GridUid == null)
+                continue;
+            if(!stationGrids.Contains(transform.GridUid.Value))
+                continue;
+
+            result.Add(transform.Coordinates);
+        }
+
+        return result.Count == 0 ? null : _random.Pick(result);
+    }
+}
