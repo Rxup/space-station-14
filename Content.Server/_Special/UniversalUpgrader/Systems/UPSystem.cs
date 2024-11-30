05e7e7c621141ebd471a782aa7389775d3c059be
sanek31 <149935707+sanek31@users.noreply.github.com>
2024-11-30 21:13:21 +0300

    Update UPSystem.cs

--- Content.Server/_Special/UniversalUpgrader/Systems/UPSystem.cs
+++ Content.Server/_Special/UniversalUpgrader/Systems/UPSystem.cs
@@ -1,6 +1,8 @@
-
+using System.Linq;
+using Content.Server.DoAfter;
 using Content.Server._Special.UniversalUpgrader.Components;
 using Content.Shared.Interaction;
+using Robust.Shared.Prototypes;
 
 namespace Content.Server._Special.UniversalUpgrader.Systems;
 
@@ -8,6 +10,7 @@ public sealed class UPSystem  : EntitySystem
 {
 
     [Dependency] private readonly EntityManager _ent = default!;
+    [Dependency] private readonly IComponentFactory _compFact = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -18,27 +21,30 @@ public sealed class UPSystem  : EntitySystem
     {
         if (!args.CanReach || args.Target is not { Valid: true } target)
             return;
-        if (entity.Comp.ProtoWhitelist != null && HasComp<MetaDataComponent>(target))
+        if (entity.Comp.ProtoWhitelist != "" && HasComp<MetaDataComponent>(target))
         {
             var z = _ent.GetComponent<MetaDataComponent>(target);
-            if (z.EntityPrototype!.ID != entity.Comp.ProtoWhitelist)
+            if (!entity.Comp.ProtoWhitelist.Split(" ").Contains(z.EntityPrototype!.ID))
                 return;
         }
 
-        Type? g = Type.GetType(entity.Comp.componentName);
+        var test = _compFact.GetRegistration(entity.Comp.componentName);
 
-        if (g != null && _ent.TryGetComponent(target, g, out var comp))
+        if (_ent.TryGetComponent(target, test.Type, out var comp))
         {
-
-            var h = comp.GetType().GetField(entity.Comp.upgradeName);
-
-            if (h != null)
+            var t = entity.Comp.upgradeName.Split(' ').Length;
+            for (int i = 0; i < t; i++)
             {
+                var un = entity.Comp.upgradeName.Split(' ')[i];
+                var uv = entity.Comp.upgradeValue.Split(' ')[i];
+                var h = comp.GetType().GetField(un);
 
-                if (h.FieldType == typeof(int)) h.SetValue(comp,(int) entity.Comp.upgradeValue);
-                if (h.FieldType == typeof(float)) h.SetValue(comp,(int) entity.Comp.upgradeValue);
-
+                if (h != null && uv != null)
+                {
+                    h.SetValue(h.FieldType, uv );
+                }
             }
+
             entity.Comp.usable -= 1;
             if (entity.Comp.usable < 0) _ent.QueueDeleteEntity(entity);
 
@@ -46,3 +52,5 @@ public sealed class UPSystem  : EntitySystem
     }
 
 }
+
+
