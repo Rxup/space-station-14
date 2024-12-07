7e8c0702d52932bec9a8dc92cd329d061b62afd8
Zack Backmen <backmen@bk.ru>
2024-12-07 04:42:46 +0300

    cleaup

--- Content.Shared/Backmen/AccessWeaponBlockerSystem/AccessWeaponBlockerSystem.cs
+++ Content.Shared/Backmen/AccessWeaponBlockerSystem/AccessWeaponBlockerSystem.cs
@@ -1,13 +1,16 @@
+using Content.Shared.Access.Components;
+using Content.Shared.Hands;
 using Content.Shared.Interaction.Events;
-using Content.Shared.Backmen.AccessWeaponBlockerSystem;
+using Content.Shared.Inventory;
+using Content.Shared.PDA;
 using Content.Shared.Weapons.Melee.Events;
 using Content.Shared.Weapons.Ranged.Systems;
-using Robust.Shared.GameStates;
 
-namespace Content.Client.Backmen.AccessWeaponBlockerSystem;
+namespace Content.Shared.Backmen.AccessWeaponBlockerSystem;
 
 public sealed class AccessWeaponBlockerSystem : EntitySystem
 {
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
     public override void Initialize()
     {
         base.Initialize();
@@ -16,7 +19,7 @@ public sealed class AccessWeaponBlockerSystem : EntitySystem
         SubscribeLocalEvent<AccessWeaponBlockerComponent, AttemptMeleeEvent>(OnMeleeAttempt);
         SubscribeLocalEvent<AccessWeaponBlockerComponent, UseAttemptEvent>(OnUseAttempt);
         SubscribeLocalEvent<AccessWeaponBlockerComponent, InteractionAttemptEvent>(OnInteractAttempt);
-        SubscribeLocalEvent<AccessWeaponBlockerComponent, ComponentHandleState>(OnAccessWeaponBlockerHandleState);
+        SubscribeLocalEvent<AccessWeaponBlockerComponent, GotEquippedHandEvent>(OnGotEquippedHand);
     }
 
     private void OnUseAttempt(EntityUid uid, AccessWeaponBlockerComponent component, ref UseAttemptEvent args)
@@ -35,15 +38,6 @@ public sealed class AccessWeaponBlockerSystem : EntitySystem
         args.Cancelled = true;
     }
 
-    private void OnAccessWeaponBlockerHandleState(EntityUid uid, AccessWeaponBlockerComponent component, ref ComponentHandleState args)
-    {
-        if (args.Current is not AccessWeaponBlockerComponentState state)
-            return;
-
-        component.CanUse = state.CanUse;
-        component.AlertText = state.AlertText;
-    }
-
     private void OnMeleeAttempt(EntityUid uid, AccessWeaponBlockerComponent component, ref AttemptMeleeEvent args)
     {
         if (component.CanUse)
@@ -60,5 +54,28 @@ public sealed class AccessWeaponBlockerSystem : EntitySystem
 
         args.Cancelled = true;
         args.Message = component.AlertText;
+    }
+
+    private void OnGotEquippedHand(EntityUid uid, AccessWeaponBlockerComponent component, ref GotEquippedHandEvent args)
+    {
+        if (!_inventorySystem.TryGetSlotEntity(args.User, "id", out var slotCardUid))
+            return;
+        var accessEntity = TryComp<PdaComponent>(slotCardUid, out var pda) && pda.ContainedId is { } pdaSlot
+            ? pdaSlot
+            : slotCardUid.Value;
+        component.CanUse = IsAnyAccess(accessEntity, component);
+        Dirty(uid, component);
+    }
+
+    private bool IsAnyAccess(EntityUid accessEntity, AccessWeaponBlockerComponent component)
+    {
+        if (!TryComp<AccessComponent>(accessEntity, out var access))
+            return false;
+        foreach (var accessTag in access.Tags)
+        {
+            if (component.Access.Contains(accessTag))
+                return true;
+        }
+        return false;
     }
 }
