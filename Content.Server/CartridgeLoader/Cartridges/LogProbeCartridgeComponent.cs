2fd63942fd69d70f4738340e19b605f90087fc13
Cat <115424457+CatBackGround@users.noreply.github.com>
2025-02-12 12:39:19 +0500

    [PORT] NanoChat

--- Content.Server/CartridgeLoader/Cartridges/LogProbeCartridgeComponent.cs
+++ Content.Server/CartridgeLoader/Cartridges/LogProbeCartridgeComponent.cs
@@ -1,8 +1,8 @@
 ﻿using Content.Shared.CartridgeLoader.Cartridges;
+using Content.Shared._CorvaxNext.CartridgeLoader.Cartridges;
 using Robust.Shared.Audio;
 
 namespace Content.Server.CartridgeLoader.Cartridges;
-
 [RegisterComponent]
 [Access(typeof(LogProbeCartridgeSystem))]
 public sealed partial class LogProbeCartridgeComponent : Component
@@ -12,10 +12,15 @@ public sealed partial class LogProbeCartridgeComponent : Component
     /// </summary>
     [DataField, ViewVariables]
     public List<PulledAccessLog> PulledAccessLogs = new();
-
     /// <summary>
     /// The sound to make when we scan something with access
     /// </summary>
     [DataField, ViewVariables(VVAccess.ReadWrite)]
     public SoundSpecifier SoundScan = new SoundPathSpecifier("/Audio/Machines/scan_finish.ogg");
+
+    /// <summary>
+    /// Corvax-Next-PDAChat: The last scanned NanoChat data, if any
+    /// </summary>
+    [DataField]
+    public NanoChatData? ScannedNanoChatData;
 }
