10232ed62088432aa67b04bd189f7eae2bcefaf7
Zack Backmen <backmen@bk.ru>
2025-02-23 19:39:47 +0300

    Update CCVars.Mood.cs

--- Content.Shared/Backmen/CCVar/CCVars.Mood.cs
+++ Content.Shared/Backmen/CCVar/CCVars.Mood.cs
@@ -5,10 +5,10 @@ namespace Content.Shared.Backmen.CCVar;
 public sealed partial class CCVars
 {
     public static readonly CVarDef<bool> MoodEnabled =
-        CVarDef.Create("mood.enabled", true, CVar.SERVERONLY);
+        CVarDef.Create("mood.enabled", true, CVar.SERVER | CVar.REPLICATED);
 
     public static readonly CVarDef<bool> MoodIncreasesSpeed =
-        CVarDef.Create("mood.increases_speed", true, CVar.SERVERONLY);
+        CVarDef.Create("mood.increases_speed", false, CVar.SERVERONLY);
 
     public static readonly CVarDef<bool> MoodDecreasesSpeed =
         CVarDef.Create("mood.decreases_speed", true, CVar.SERVERONLY);
