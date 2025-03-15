8835d1766691e17a5337c17bd002be69d29f8eff
FaDeOkno <logkedr18@gmail.com>
2025-03-15 04:20:45 +0400

    Research Rework (Squash)
    add(src): Crimson cost color for PrereqsMet Availability
    
    whoopsie
    
    style(src): Rename ResearchAvailability.cs
    
    add(src): Shitmed researches compability
    
    add(src): More colors, yuppie.
    
    real
    
    ops
    
    webedit
    
    lol
    
    style(src): Better commenting
    
    style(src): Remove unused variables
    
    style(src): Remove manually set field name
    
    fix(lang): Fix spelling mistake
    
    refactor(src): Separate `FancyTechnologyInfoPanel` initialization
    
    style(src): Other suggested style changes
    
    style(src): Simplify single-lined functions
    
    refactor(src): Better BUI state building
    
    refactor(src): Getting rid of LocalState and useless prototype enumerating in menu
    
    remove(src): Remove discipline field from window
    
    fix(proto): Fix linter
    
    fixes research
    
    add(proto): Added positions and prereqs to goob tech + moved svalin to exsisting T1 research
    
    fix(src)!: FIXED SOUND SPAM
    
    refactor(src): Replace disposing with removing
    
    fix(proto): Remove `WaveParticleHarnessing` dependency
    
    add(lang): Prereqs display locale
    
    style: e
    
    add(src, ui): Tech prereqs display
    
    remove(src): Remove last (i hope) used tier-related code
    
    fix(src): Misplaced } fix
    
    fix(src): Fixex bot changes
    
    Update Content.Client/_Goobstation/Research/UI/ResearchesContainerPanel.cs
    
    Update Content.Client/_Goobstation/Research/UI/FancyResearchConsoleMenu.xaml.cs
    
    style: Minor spelling mistake moment
    
    Update Content.Client/_Goobstation/Research/UI/ResearchesContainerPanel.cs
    
    Update Content.Shared/_Goobstation/Research/SharedResearchSystem.Goob.cs
    
    tweak(proto): Increased cost of wizden T3 techs
    
    fix(proto): Fix shuttlecraft requiring industrial engineering instead of mechanical compression
    
    tweak(proto): Smol experimental tech tweaks
    
    tweak(lang): Smol locale tweak
    
    fix(src): Real refresh spam fix
    
    fix(src): Important fixes
    
    add(lang): locale moment
    
    fix(src): Refreshing info panel aftel handling BUI state
    
    tweak(src): Make console show research percentage instead of highest tier
    
    tweak(ui): Minor fixes
    
    add(src, ui): Technology info panel now shows cost of the tech
    
    add(proto): Wizden tech arrangement in UI + discipline names
    
    add(texture): Static background sprite
    
    remove(src): Remove discipline locking
    
    add(src): Working server part and BUI
    
    fix(src, ui): Research Item UI element fix
    
    add(src, ui): UI stuff - menu and required elements
    
    add(src): Research avaibility enum
    
    add(src): Prototype and BUI state fields
    
    Co-Authored-By: Aiden <aiden@djkraz.com>
    Co-Authored-By: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Content.Client/Research/UI/ResearchConsoleBoundUserInterface.cs
+++ Content.Client/Research/UI/ResearchConsoleBoundUserInterface.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Content.Client._Goobstation.Research.UI;
 using Content.Shared.Research.Components;
 using Content.Shared.Research.Prototypes;
 using JetBrains.Annotations;
@@ -10,7 +12,7 @@ namespace Content.Client.Research.UI;
 public sealed class ResearchConsoleBoundUserInterface : BoundUserInterface
 {
     [ViewVariables]
-    private ResearchConsoleMenu? _consoleMenu;
+    private FancyResearchConsoleMenu? _consoleMenu;  // Goobstation R&D Console rework - ResearchConsoleMenu -> FancyResearchConsoleMenu
 
     public ResearchConsoleBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
@@ -22,8 +24,9 @@ public sealed class ResearchConsoleBoundUserInterface : BoundUserInterface
 
         var owner = Owner;
 
-        _consoleMenu = this.CreateWindow<ResearchConsoleMenu>();
+        _consoleMenu = this.CreateWindow<FancyResearchConsoleMenu>();   // Goobstation R&D Console rework - ResearchConsoleMenu -> FancyResearchConsoleMenu
         _consoleMenu.SetEntity(owner);
+        _consoleMenu.OnClose += () => _consoleMenu = null;
 
         _consoleMenu.OnTechnologyCardPressed += id =>
         {
@@ -46,8 +49,8 @@ public sealed class ResearchConsoleBoundUserInterface : BoundUserInterface
         if (State is not ResearchConsoleBoundInterfaceState rState)
             return;
 
-        _consoleMenu?.UpdatePanels(rState);
-        _consoleMenu?.UpdateInformationPanel(rState);
+        _consoleMenu?.UpdatePanels(rState.Researches);
+        _consoleMenu?.UpdateInformationPanel(rState.Points);
     }
 
     protected override void UpdateState(BoundUserInterfaceState state)
@@ -56,7 +59,14 @@ public sealed class ResearchConsoleBoundUserInterface : BoundUserInterface
 
         if (state is not ResearchConsoleBoundInterfaceState castState)
             return;
-        _consoleMenu?.UpdatePanels(castState);
-        _consoleMenu?.UpdateInformationPanel(castState);
+
+        // Goobstation checks added
+        // Thats for avoiding refresh spam when only points are updated
+        if (_consoleMenu == null)
+            return;
+        if (!_consoleMenu.List.SequenceEqual(castState.Researches))
+            _consoleMenu.UpdatePanels(castState.Researches);
+        if (_consoleMenu.Points != castState.Points)
+            _consoleMenu.UpdateInformationPanel(castState.Points);
     }
 }
