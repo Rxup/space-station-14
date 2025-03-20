LaCumbiaDelCoronavirus/KS16

Reflect and esword stuff (#1137)
Co-authored-by: Preston Smith <92108534+thetolbean@users.noreply.github.com>
Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>

Content.Client/_White/Standing/LayingDownSystem.cs
@@ -1,3 +1,4 @@
+using Content.Client._White.Animations;
 using Content.Shared._White.Standing;
 using Content.Shared.Buckle;
 using Content.Shared.Rotation;
@@ -64,6 +65,12 @@ public sealed class LayingDownSystem : SharedLayingDownSystem
 
         if (!TryComp<TransformComponent>(uid, out var transform) || !TryComp<RotationVisualsComponent>(uid, out var rotationVisuals))
             return;
+
+        if (_animation.HasRunningAnimation(uid, FlippingComponent.AnimationKey))
+        {
+            RemComp<FlippingComponent>(uid);
+            _animation.Stop(uid, FlippingComponent.AnimationKey);
+        }
 
         var rotation = transform.LocalRotation + (_eyeManager.CurrentEye.Rotation - (transform.LocalRotation - transform.WorldRotation));
 

Rxup/space-station-14

Content.Client/Backmen/Standing/LayingDownSystem.cs
@@ -1,3 +1,4 @@
+using Content.Client._White.Animations;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Backmen.CCVar;
 using Content.Shared.Backmen.Standing;
@@ -40,6 +41,12 @@ public sealed class LayingDownSystem : SharedLayingDownSystem
     {
         if(_animation.HasRunningAnimation(ent, "rotate"))
             return;
+
+        if (_animation.HasRunningAnimation(ent.Owner, FlippingComponent.AnimationKey))
+        {
+            RemComp<FlippingComponent>(ent);
+            _animation.Stop(ent.Owner, FlippingComponent.AnimationKey);
+        }
 
         if (!TryComp<SpriteComponent>(ent, out var sprite))
         {