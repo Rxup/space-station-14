LaCumbiaDelCoronavirus/KS16

Martial Arts Part 1: Grabbing aka "Grab intent" [speedmerge] (#1839)
* grab and  bag

* linter fail

* Fix wizcode'

* fixed grub and orphaning

* stupid file

* removed annoying logging

* removed stupid debounce shit

* :trollface

* oops left a logger.debug

* fix portals

* clear variable for throwback lowered it

* i gotchu mocho

* ling must grab to absorb

* tanks john

* did review cuff still weird ig

Content.Server/_White/Traits/Assorted/LayingDownModifierSystem.cs
@@ -1,0 +1,21 @@
+using Content.Shared._White.Standing;
+
+namespace Content.Server._White.Traits.Assorted;
+
+public sealed class LayingDownModifierSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<LayingDownModifierComponent, ComponentStartup>(OnStartup);
+    }
+
+    private void OnStartup(EntityUid uid, LayingDownModifierComponent component, ComponentStartup args)
+    {
+        if (!TryComp<LayingDownComponent>(uid, out var layingDown))
+            return;
+
+        layingDown.StandingUpTime *= component.LayingDownCooldownMultiplier;
+        layingDown.SpeedModify *= component.DownedSpeedMultiplierMultiplier;
+    }
+}

Rxup/space-station-14

Content.Server/_White/Traits/Assorted/LayingDownModifierSystem.cs
@@ -1,0 +1,21 @@
+using Content.Shared.Backmen.Standing;
+
+namespace Content.Server._White.Traits.Assorted;
+
+public sealed class LayingDownModifierSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<LayingDownModifierComponent, ComponentStartup>(OnStartup);
+    }
+
+    private void OnStartup(EntityUid uid, LayingDownModifierComponent component, ComponentStartup args)
+    {
+        if (!TryComp<LayingDownComponent>(uid, out var layingDown))
+            return;
+
+        layingDown.StandingUpTime *= component.LayingDownCooldownMultiplier;
+        layingDown.SpeedModify *= component.DownedSpeedMultiplierMultiplier;
+    }
+}