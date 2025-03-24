9fea584899d50990e2f418510ed6fa48c706b002
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-03-24 21:39:58 +0300

    [Feature] Decal Grenade (#1117)
    * [Feature] Decal Grenade
    
    * remove contraband
    
    * better cooode
    
    * ugh
    
    * something
    
    * spizdil confetti
    
    * maybe work idk?
    
    * Перезапуск тестов

---
+++ Content.Server/Backmen/Explosion/Components/DecalGrenadeComponent.cs
@@ -0,0 +1,40 @@
+using Content.Server.Backmen.Explosion.EntitySystems;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+
+namespace Content.Server.Backmen.Explosion.Components;
+
+/// <summary>
+/// Grenades that, when triggered, explode into decals.
+/// </summary>
+[RegisterComponent, Access(typeof(DecalGrenadeSystem))]
+public sealed partial class DecalGrenadeComponent : Component
+{
+    [Dependency] private readonly IRobustRandom _random = default!;
+
+    /// <summary>
+    /// The kinds of decals to spawn on explosion.
+    /// </summary>
+    [DataField]
+    public List<string> DecalPrototypes = new();
+
+    /// <summary>
+    /// The number of decals to spawn upon explosion.
+    /// </summary>
+    [DataField]
+    public int DecalCount = 25;
+
+    /// <summary>
+    /// The radius in which decals will spawn around the explosion center.
+    /// </summary>
+    [DataField]
+    public float DecalRadius = 3f;
+
+    public string? GetRandomDecal()
+    {
+        if (DecalPrototypes == null || DecalPrototypes.Count == 0)
+            return null;
+
+        return DecalPrototypes[_random.Next(DecalPrototypes.Count)];
+    }
+}
