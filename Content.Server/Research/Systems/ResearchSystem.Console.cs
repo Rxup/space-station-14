22725c416e9474c2b4ef0ce1ba06269a7e3ee624
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-04-04 00:52:43 +0300

    [Port] RND Research Tree (#1183)
    * Research Rework (Squash)
    
    add(src): Crimson cost color for PrereqsMet Availability
    
    whoopsie
    
    style(src): Rename ResearchAvailability.cs
    
    add(src): Shitmed researches compability
    
    add(src): More colors, yuppie.
    
    real
    
    ops
    
    webedit
    
    lol
    
    style(src): Better commenting
    
    style(src): Remove unused variables
    
    style(src): Remove manually set field name
    
    fix(lang): Fix spelling mistake
    
    refactor(src): Separate `FancyTechnologyInfoPanel` initialization
    
    style(src): Other suggested style changes
    
    style(src): Simplify single-lined functions
    
    refactor(src): Better BUI state building
    
    refactor(src): Getting rid of LocalState and useless prototype enumerating in menu
    
    remove(src): Remove discipline field from window
    
    fix(proto): Fix linter
    
    fixes research
    
    add(proto): Added positions and prereqs to goob tech + moved svalin to exsisting T1 research
    
    fix(src)!: FIXED SOUND SPAM
    
    refactor(src): Replace disposing with removing
    
    fix(proto): Remove `WaveParticleHarnessing` dependency
    
    add(lang): Prereqs display locale
    
    style: e
    
    add(src, ui): Tech prereqs display
    
    remove(src): Remove last (i hope) used tier-related code
    
    fix(src): Misplaced } fix
    
    fix(src): Fixex bot changes
    
    Update Content.Client/_Goobstation/Research/UI/ResearchesContainerPanel.cs
    
    Update Content.Client/_Goobstation/Research/UI/FancyResearchConsoleMenu.xaml.cs
    
    style: Minor spelling mistake moment
    
    Update Content.Client/_Goobstation/Research/UI/ResearchesContainerPanel.cs
    
    Update Content.Shared/_Goobstation/Research/SharedResearchSystem.Goob.cs
    
    tweak(proto): Increased cost of wizden T3 techs
    
    fix(proto): Fix shuttlecraft requiring industrial engineering instead of mechanical compression
    
    tweak(proto): Smol experimental tech tweaks
    
    tweak(lang): Smol locale tweak
    
    fix(src): Real refresh spam fix
    
    fix(src): Important fixes
    
    add(lang): locale moment
    
    fix(src): Refreshing info panel aftel handling BUI state
    
    tweak(src): Make console show research percentage instead of highest tier
    
    tweak(ui): Minor fixes
    
    add(src, ui): Technology info panel now shows cost of the tech
    
    add(proto): Wizden tech arrangement in UI + discipline names
    
    add(texture): Static background sprite
    
    remove(src): Remove discipline locking
    
    add(src): Working server part and BUI
    
    fix(src, ui): Research Item UI element fix
    
    add(src, ui): UI stuff - menu and required elements
    
    add(src): Research avaibility enum
    
    add(src): Prototype and BUI state fields
    
    Co-Authored-By: Aiden <aiden@djkraz.com>
    Co-Authored-By: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * smthg like fix
    
    * transfer
    
    * reform tech
    
    * smthg
    
    * smthg like what looks better
    
    * ru ftl
    
    * fucking fuck
    
    * eng ftl
    
    * ok
    
    * fix
    
    * yaml linter fix
    
    * yaml linet fix2
    
    * yaml linter fix3
    
    * build fix mb
    
    * woops
    
    * build fix2
    
    * build fix3
    
    ---------
    
    Co-authored-by: FaDeOkno <logkedr18@gmail.com>
    Co-authored-by: Aiden <aiden@djkraz.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Content.Server/Research/Systems/ResearchSystem.Console.cs
+++ Content.Server/Research/Systems/ResearchSystem.Console.cs
@@ -69,11 +69,13 @@ public sealed partial class ResearchSystem
 
     private void UpdateConsoleInterface(EntityUid uid, ResearchConsoleComponent? component = null, ResearchClientComponent? clientComponent = null)
     {
+        // Goobstation R&D Console Rework commented and replaced with other func
+        UpdateFancyConsoleInterface(uid, component, clientComponent);
+
+        /*
         if (!Resolve(uid, ref component, ref clientComponent, false))
             return;
-
         ResearchConsoleBoundInterfaceState state;
-
         if (TryGetClientServer(uid, out _, out var serverComponent, clientComponent))
         {
             var points = clientComponent.ConnectedToServer ? serverComponent.Points : 0;
@@ -83,8 +85,8 @@ public sealed partial class ResearchSystem
         {
             state = new ResearchConsoleBoundInterfaceState(default);
         }
-
         _uiSystem.SetUiState(uid, ResearchConsoleUiKey.Key, state);
+        */
     }
 
     private void OnPointsChanged(EntityUid uid, ResearchConsoleComponent component, ref ResearchServerPointsChangedEvent args)
--- Resources/Locale/en-US/backmen/research/technologies.ftl
+++
@@ -1,5 +0,0 @@
-research-technology-genetic-weaponry = Genetic Weaponry
-research-technology-metempsychosis = Metempsychosis
-research-technology-navigation = Navigation
-research-technology-psionic-countermeasures = Psionic Countermeasures
-research-technology-teleportation = Teleportation
