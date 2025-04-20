0e414dd347b197717a841eeee4d07ec334672e58
Roudenn <romabond091@gmail.com>
2025-04-20 19:39:08 +0300

    Update CCVars.Surgery.cs

--- Content.Shared/Backmen/CCVar/CCVars.Surgery.cs
+++ Content.Shared/Backmen/CCVar/CCVars.Surgery.cs
@@ -6,5 +6,5 @@ public sealed partial class CCVars
 {
 
     public static readonly CVarDef<bool> CanOperateOnSelf =
-        CVarDef.Create("surgery.can_operate_on_self", false, CVar.SERVERONLY);
+        CVarDef.Create("surgery.can_operate_on_self", true, CVar.SERVERONLY);
 }
