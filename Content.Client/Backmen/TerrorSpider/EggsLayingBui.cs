64a5763f5fbbb0e904d44226255363c7b253e6c9
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-05-01 00:13:13 +0300

    base

---
+++ Content.Client/Backmen/TerrorSpider/EggsLayingBui.cs
@@ -0,0 +1,33 @@
+﻿using Content.Shared.Backmen.Antags.TerrorSpider;
+using Robust.Client.Graphics;
+using Robust.Client.Input;
+using Robust.Client.UserInterface;
+using JetBrains.Annotations;
+
+namespace Content.Client.Backmen.Antags.TerrorSpider;
+
+[UsedImplicitly]
+public sealed class EggsLayingBui : BoundUserInterface
+{
+    [Dependency] private readonly IInputManager _inputManager = default!;
+    [Dependency] private readonly IClyde _displayManager = default!;
+
+    private EggsLayingMenu? _menu;
+    public EggsLayingBui(EntityUid owner, Enum uiKey) : base(owner, uiKey) => IoCManager.InjectDependencies(this);
+    protected override void Open()
+    {
+        _menu = this.CreateWindow<EggsLayingMenu>();
+        _menu.OnClose += Close;
+        _menu.EggChosen += (egg) =>
+        {
+            SendMessage(new EggsLayingBuiMsg() { Egg = egg });
+            _menu.Close();
+            Close();
+        };
+        var vpSize = _displayManager.ScreenSize;
+        _menu.OpenCenteredAt(_inputManager.MouseScreenPosition.Position / vpSize);
+    }
+    protected override void UpdateState(BoundUserInterfaceState? state)
+    {
+    }
+}
---
+++ Content.Shared/Backmen/TerrorSpider/TerrorSpiderComponents.cs
@@ -0,0 +1,52 @@
+﻿using Content.Shared.Actions;
+using Content.Shared.DoAfter;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Backmen.Antags.TerrorSpider;
+[RegisterComponent]
+public sealed partial class StealthOnWebComponent : Component
+{
+    [DataField]
+    public int Collisions = 0;
+}
+[RegisterComponent]
+public sealed partial class EggHolderComponent : Component
+{
+    [DataField]
+    public int Counter = 0;
+}
+[RegisterComponent]
+public sealed partial class HasEggHolderComponent : Component
+{
+}
+[RegisterComponent]
+public sealed partial class TerrorPrincessComponent : Component
+{
+}
+public sealed partial class EggInjectionEvent : EntityTargetActionEvent
+{
+}
+[Serializable, NetSerializable]
+public sealed partial class EggInjectionDoAfterEvent : SimpleDoAfterEvent
+{
+}
+[Serializable, NetSerializable]
+public enum EggsLayingUiKey : byte
+{
+    Key
+}
+
+public sealed partial class EggsLayingEvent : InstantActionEvent
+{
+
+}
+[Serializable, NetSerializable]
+public sealed class EggsLayingBuiMsg : BoundUserInterfaceMessage
+{
+    public EntProtoId Egg { get; set; }
+}
+[Serializable, NetSerializable]
+public sealed class EggsLayingBuiState : BoundUserInterfaceState
+{
+}
