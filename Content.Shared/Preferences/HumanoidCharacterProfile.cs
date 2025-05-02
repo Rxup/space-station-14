f03af6ee1593f0e9ea1b650c2708f5debda250a0
Trest <144359854+trest100@users.noreply.github.com>
2025-05-02 18:49:34 +0300

    Update HumanoidCharacterProfile.cs

--- Content.Shared/Preferences/HumanoidCharacterProfile.cs
+++ Content.Shared/Preferences/HumanoidCharacterProfile.cs
@@ -31,7 +31,7 @@ namespace Content.Shared.Preferences
 
         public const int MaxNameLength = 32;
         public const int MaxLoadoutNameLength = 32;
-        public const int MaxDescLength = 512;
+        public const int MaxDescLength = 5200;
 
         /// <summary>
         /// Job preferences for initial spawn.
