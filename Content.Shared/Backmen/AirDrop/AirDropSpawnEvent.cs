17d6da3015a5111299a374a36f359a63305876ff
Zack Backmen <backmen@bk.ru>
2025-07-14 01:31:42 +0300

    AirDrop system via code

---
+++ Content.Shared/Backmen/AirDrop/AirDropSpawnEvent.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared.EntityTable.EntitySelectors;
+using Robust.Shared.Map;
+
+namespace Content.Shared.Backmen.AirDrop;
+
+public sealed class AirDropSpawnEvent : HandledEntityEventArgs
+{
+    public MapCoordinates Pos { get; set; }
+}
+public sealed class AirDropTargetSpawnEvent : HandledEntityEventArgs
+{
+    public MapCoordinates Pos { get; set; }
+}
+public sealed class AirDropItemSpawnEvent : HandledEntityEventArgs
+{
+    public required EntityTableSelector DropTable { get; set; }
+    public EntityUid SupplyPod { get; set; }
+    public bool ForceOpenSupplyDrop { get; set; } = false;
+}
