7372b98859bc423824e05e09e901b8357a8a5f60
Zack Backmen <backmen@bk.ru>
2026-01-13 22:32:38 +0300

    FixBosses

---
+++ Content.Server/Backmen/Boss/PolymorphBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Content.Server.Destructible;
+using Content.Server.Destructible.Thresholds.Behaviors;
+using Content.Server.Polymorph.Systems;
+using Content.Shared.Polymorph;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+
+namespace Content.Server.Backmen.Boss;
+
+[Serializable]
+[DataDefinition]
+public sealed partial class PolymorphBehavior : IThresholdBehavior
+{
+    [DataField(required: true)]
+    public List<ProtoId<PolymorphPrototype>> Prototypes = [];
+
+    [DataField]
+    public float Chance = 1.0f;
+
+    public void Execute(EntityUid owner, DestructibleSystem system, EntityUid? cause = null)
+    {
+        if (!system.Random.Prob(Chance))
+            return;
+
+        var proto = system.Random.Pick(Prototypes);
+
+        var pSys = system.EntityManager.System<PolymorphSystem>();
+        pSys.PolymorphEntity(owner, proto);
+    }
+}
---
+++ Content.Server/Backmen/Boss/TimedPolymorphComponent.cs
@@ -0,0 +1,31 @@
+﻿using Content.Shared.Polymorph;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Server.Backmen.Boss;
+
+[RegisterComponent]
+[AutoGenerateComponentPause]
+public sealed partial class TimedPolymorphComponent : Component
+{
+    [DataField]
+    public List<ProtoId<PolymorphPrototype>> Prototypes = [];
+
+    /// <summary>
+    /// Chance of an entity being spawned at the end of each interval.
+    /// </summary>
+    [DataField]
+    public float Chance = 1.0f;
+
+    /// <summary>
+    /// Length of the interval between spawn attempts.
+    /// </summary>
+    [DataField]
+    public TimeSpan IntervalSeconds = TimeSpan.FromSeconds(60);
+
+    /// <summary>
+    /// The time at which the current interval will have elapsed and entities may be spawned.
+    /// </summary>
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoPausedField]
+    public TimeSpan NextFire = TimeSpan.Zero;
+}
