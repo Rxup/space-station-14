039096b058553b7e48ea2a5fb78cac4e7fd6e9e9
Whatstone <166147148+whatston3@users.noreply.github.com>
2026-07-20 14:29:08 -0400

    Bugfix: Fix some spawned entities parenting to vehicles. (#44764)
    * SpawnAtPosition gaming
    
    * SpawnAtPosition gaming: gun edition
    
    * spawnnexttoordrop, restore unused gun dep.
    
    (cherry picked from commit 887e80fd08a196228765b5b70c72b80d800203f6)

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Revolver.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Revolver.cs
@@ -369,7 +369,7 @@ public partial class SharedGunSystem
                 if (chamber == true)
                 {
                     // Pretend it's always been there.
-                    ammoEnt = Spawn(ent.Comp.FillPrototype, args.Coordinates);
+                    ammoEnt = SpawnAtPosition(ent.Comp.FillPrototype, args.Coordinates);
 
                     if (!_netManager.IsClient)
                     {
@@ -392,7 +392,7 @@ public partial class SharedGunSystem
 
                 // Mark cartridge as spent and if it's caseless delete from the chamber slot.
                 SetCartridgeSpent(ammoEnt.Value, cartridge, true);
-                var spawned = Spawn(cartridge.Prototype, args.Coordinates);
+                var spawned = SpawnAtPosition(cartridge.Prototype, args.Coordinates);
                 args.Ammo.Add((spawned, EnsureComp<AmmoComponent>(spawned)));
 
                 if (cartridge.DeleteOnSpawn)
