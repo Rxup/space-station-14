c8bf6408610eaf9397217570413f2f8cc37593ab
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2026-07-24 18:42:11 +1000

    Fix ExpireIdCard (#44567)
    (cherry picked from commit d8380807eb1cf29bd53f858d000932da0bcf3f0a)

--- Content.Server/Access/Systems/IdCardSystem.cs
+++ Content.Server/Access/Systems/IdCardSystem.cs
@@ -99,12 +99,10 @@ public sealed partial class IdCardSystem : SharedIdCardSystem
         }
     }
 
-    public override void ExpireId(Entity<ExpireIdCardComponent> ent)
+    public override bool ExpireId(Entity<ExpireIdCardComponent> ent)
     {
-        if (ent.Comp.Expired)
-            return;
-
-        base.ExpireId(ent);
+        if (!base.ExpireId(ent))
+            return false;
 
         if (ent.Comp.ExpireMessage != null)
         {
@@ -115,5 +113,7 @@ public sealed partial class IdCardSystem : SharedIdCardSystem
                 ChatTransmitRange.Normal,
                 true);
         }
+
+        return true;
     }
 }
