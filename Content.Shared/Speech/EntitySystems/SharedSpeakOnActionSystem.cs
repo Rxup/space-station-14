ea6cc3027d8411d46051b76fc675408ed7f6575c
Zack Backmen <backmen@bk.ru>
2026-08-13 07:09:24 +0300

    ooops

--- Content.Shared/Speech/EntitySystems/SharedSpeakOnActionSystem.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Shared.Speech.EntitySystems;
-
-/// <summary>
-/// Once the chat refactor has happened, move the code from
-/// <see cref="Content.Server.Speech.EntitySystems.SpeakOnUseSystem"/>
-/// to here and set this class to sealed.
-/// </summary>
-public abstract class SharedSpeakOnActionSystem : EntitySystem;
